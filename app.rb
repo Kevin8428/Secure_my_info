2fc9a849ac2e1d82c47cfd3cf25888408a53bc29
kevin deutscher <kevin.m.deutscher@gmail.com>
2015-12-03 12:23:32 -0600

    framework update

--- app.rb
+++ app.rb
@@ -1,2 +1,16 @@
-require 'bundler'
-Bundler.require
+require 'bundler' #require the bundler
+Bundler.require #bundle stuff
+
+
+# establishing connection to postgresql db
+ActiveRecord::Base.establish_connection(
+  :database => 'bee_crypt_bzz',
+  :adapter => 'postgresql'
+)
+
+# basic template routes
+
+get '/' do
+  # get some resource
+  return {:hello => 'world'}.to_json
+end
---
+++ models/account.rb
@@ -0,0 +1,6 @@
+class Account < ActiveRecord::Base #this needs to be account singular because table is 'accounts'
+
+
+
+
+end
